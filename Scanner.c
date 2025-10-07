262c43e2c4b6d40f1e91ff031be3896da08a1486
Your Nameclks <you@example.com>
2025-10-07 16:09:21 -0600

    Added documentation to Scanner.[hc]

--- Scanner.c
+++ Scanner.c
@@ -5,94 +5,137 @@
 #include "Scanner.h"
 #include "error.h"
 
-typedef struct {
+typedef struct
+{
+  // End of string flag 1 = no more tokens
   int eos;
+  // Copy of original string
   char *str;
+  // Current position in the string
   char *pos;
+  // Current token
   char *curr;
-} *ScannerRep;
+} *ScannerRep; // Private: Internal Representation
 
-extern Scanner newScanner(char *s) {
-  ScannerRep r=(ScannerRep)malloc(sizeof(*r));
+extern Scanner newScanner(char *s)
+{
+  ScannerRep r = (ScannerRep)malloc(sizeof(*r));
   if (!r)
     ERROR("malloc() failed");
-  r->eos=0;
-  r->str=strdup(s);
-  r->pos=r->str;
-  r->curr=0;
+  r->eos = 0;
+  r->str = strdup(s);
+  r->pos = r->str;
+  r->curr = 0;
   return r;
 }
 
-extern void freeScanner(Scanner scan) {
-  ScannerRep r=scan;
+extern void freeScanner(Scanner scan)
+{
+  ScannerRep r = scan;
   free(r->str);
   if (r->curr)
     free(r->curr);
   free(r);
 }
-
-static char *thru(char *p, char *q) {
-  for (; *p && strchr(q,*p); p++);
+/**
+ * Adcances pointer (p) through any characters found in string (q) stops at first character not in q
+ */
+static char *thru(char *p, char *q)
+{
+  for (; *p && strchr(q, *p); p++)
+    ;
+  return p;
+}
+/**
+ * Advances pointer p up to any character found in string q
+Stops at first character that IS in q
+ */
+static char *upto(char *p, char *q)
+{
+  for (; *p && !strchr(q, *p); p++)
+    ;
   return p;
 }
 
-static char *upto(char *p, char *q) {
-  for (; *p && !strchr(q,*p); p++);
-  return p;
-}
+static char *wsthru(char *p) { return thru(p, " \t"); }
+static char *wsupto(char *p) { return upto(p, " \t"); }
 
-static char *wsthru(char *p) { return thru(p," \t"); }
-static char *wsupto(char *p) { return upto(p," \t"); }
-
-extern char *nextScanner(Scanner scan) {
-  ScannerRep r=scan;
+extern char *nextScanner(Scanner scan)
+{
+  ScannerRep r = scan;
+  // Check if scanner is at end of string
   if (r->eos)
     return 0;
-  char *old=wsthru(r->pos);
-  char *new=wsupto(old);
-  int size=new-old;
-  if (size==0) {
-    r->eos=1;
+
+  // At the current position Skip through any whitespace
+  char *old = wsthru(r->pos);
+  // From position(not a whitespace) Finds next whitespace
+  char *new = wsupto(old);
+  // Size is equal to the number of tokens in the input string
+  int size = new - old;
+
+  // Checking if there are tokens
+  if (size == 0)
+  {
+    r->eos = 1;
     return 0;
   }
+
   if (r->curr)
     free(r->curr);
-  r->curr=(char *)malloc(size+1);
+
+  r->curr = (char *)malloc(size + 1);
+
   if (!r->curr)
     ERROR("malloc() failed");
-  memmove(r->curr,old,size);
-  (r->curr)[size]=0;
-  r->pos=new;
+
+  memmove(r->curr, old, size);
+
+  (r->curr)[size] = 0;
+  r->pos = new;
   return r->curr;
 }
 
-extern char *currScanner(Scanner scan) {
-  ScannerRep r=scan;
+extern char *currScanner(Scanner scan)
+{
+  ScannerRep r = scan;
+  // Check if at the end of the string
   if (r->eos)
     return 0;
+  // Return current token if valid
   if (r->curr)
     return r->curr;
+  // Call nextScanner to get next token (might happen if scanner is somehow before start of string)
   return nextScanner(scan);
 }
 
-extern int cmpScanner(Scanner scan, char *s) {
-  ScannerRep r=scan;
+extern int cmpScanner(Scanner scan, char *s)
+{
+  ScannerRep r = scan;
+
+  // Get the current token
   currScanner(scan);
+  // Check if at the end of the string
   if (r->eos)
     return 0;
-  if (strcmp(s,r->curr))
+  // call strcmp to compare the current token to given string
+  if (strcmp(s, r->curr))
     return 0;
   return 1;
 }
 
-extern int eatScanner(Scanner scan, char *s) {
-  int r=cmpScanner(scan,s);
+extern int eatScanner(Scanner scan, char *s)
+{
+  // Call cmpScanner to check equality of current token and given string
+  int r = cmpScanner(scan, s);
+  // If there is a match call nextScanner to advance scanner
   if (r)
     nextScanner(scan);
   return r;
 }
 
-extern int posScanner(Scanner scan) {
-  ScannerRep r=scan;
-  return (r->pos)-(r->str);
+extern int posScanner(Scanner scan)
+{
+  ScannerRep r = scan;
+  return (r->pos) - (r->str);
 }
--- Scanner.h
+++ Scanner.h
@@ -1,15 +1,59 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+// Public users see it as only an opaque pointer and cannot see the the internal structure for encapsulations purposes.
 typedef void *Scanner;
 
+/**
+ * @brief Creates a new scanner
+ *
+ * @param s String to be parsed using the scanner
+ * @return A new Scanner ready to parse
+ */
 extern Scanner newScanner(char *s);
+
+/**
+ * @brief Cleans up Scanner, freeing all allocated memory
+ * @param scan Scanner object
+ * @return void
+ */
 extern void freeScanner(Scanner scan);
 
+/**
+ * @brief Get next token and advances the scanner
+ * @param scan Scanner object
+ * @return Next token in the string
+ */
 extern char *nextScanner(Scanner scan);
+
+/**
+ * @brief Peek at the current Token with out advancing the scanner
+ * @param scan Scanner object
+ * @return Current token
+ */
 extern char *currScanner(Scanner scan);
+
+/**
+ * @brief Compares current token to a given string. Does not advance the scanner
+ * @param scan Scanner object
+ * @param s Token string to be compared to current token
+ * @return int representing the equality of the two strings. 1 if a match and 0 if not
+ */
 extern int cmpScanner(Scanner scan, char *s);
+
+/**
+ * @brief Compares current token to a given string. If equal advance the scanner
+ * @param scan Scanner object
+ * @param s Token string to be compared to current token
+ * @return int representing the equality of the two strings. 1 if a match and 0 if not
+ */
 extern int eatScanner(Scanner scan, char *s);
+
+/**
+ * @brief Get Current Position
+ * @param scan Scanner object
+ * @return Number of characters into the originial string the scanner is
+ */
 extern int posScanner(Scanner scan);
 
 #endif
